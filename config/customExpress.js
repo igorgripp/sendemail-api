44ec096518742deec2be68b4865f0fdc44fd6ee8
Igor Gripp <igorgripp@gmail.com>
2022-01-16 08:46:23 -0300

    Include CORS

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 module.exports = () => {
   const app = express();
 
+  app.use(cors({
+    origin: "https://www.igfullstack.com.br"
+  }));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   return app;
