7f0186794336590b938e4f0025d87dac43a3867f
Igor Gripp <igorgripp@gmail.com>
2022-01-17 14:24:15 -0300

    Adjusts of variables

--- index.js
+++ index.js
@@ -11,26 +11,6 @@ const user = process.env.EMAIL_USER;
 const pass = process.env.EMAIL_PASSWORD;
 
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://sendemail-igfullstack.herokuapp.com/');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
 app.get("/", (req, res) => {
   res.send('Welcome to API sendMail'); 
 })
