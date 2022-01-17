ec4827b2f4d3ec90886cc6a4a345b78814a53616
Igor Gripp <igorgripp@gmail.com>
2022-01-17 14:48:44 -0300

    Adjusts of variables

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const user = process.env.EMAIL_USER;
 const pass = process.env.EMAIL_PASSWORD;
 
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+      'Access-Control-Allow-Header',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).send({});
+  }
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send('Welcome to API sendMail'); 
 })
